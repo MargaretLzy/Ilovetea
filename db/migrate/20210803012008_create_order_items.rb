557d3fb085831aa067bc72626f130ea21c183d21
MargaretLzy <43190680+MargaretLzy@users.noreply.github.com>
2021-08-03 10:57:49 -0400

    migrate

--- db/migrate/20210803012008_create_order_items.rb
+++ db/migrate/20210803012008_create_order_items.rb
@@ -2,8 +2,9 @@ class CreateOrderItems < ActiveRecord::Migration[6.1]
   def change
     create_table :order_items do |t|
       t.belongs_to :order, null: false, foreign_key: true
-      t.belongs_to :menuitem, null: false, foreign_key: true
+      t.belongs_to :menu_items, null: false, foreign_key: true
       t.integer :quantity
+      t.decimal :unit_price
       t.timestamps
     end
   end
--- db/migrate/20210803012322_create_menu_items.rb
+++ db/migrate/20210803012322_create_menu_items.rb
@@ -1,7 +1,10 @@
 class CreateMenuItems < ActiveRecord::Migration[6.1]
   def change
     create_table :menu_items do |t|
-
+      t.string :name
+      t.string :image
+      t.decimal :price
+      t.text :description
       t.timestamps
     end
   end
