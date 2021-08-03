310350c02bbcbfacef24b663e463143daa4ecb74
MargaretLzy <43190680+MargaretLzy@users.noreply.github.com>
2021-08-03 11:41:36 -0400

    seed

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,2 +1,18 @@
 class MenuItemsController < ApplicationController
+    def index
+        items = MenuItem.all.order('id')
+        render json: items
+      end
+    
+      def create
+        items = MenuItem.create(item_params)
+        render json: items
+      end
+    
+     
+      private
+    
+      def item_params
+        params.permit(:name, :image, :price)
+      end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+menu_item =[
+    {
+        name:"Matcha Green Tea",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Matcha_green_tea.jpg/1200px-Matcha_green_tea.jpg",
+        price: 6.4,
+        description:"Matcha milk tea is a refreshing beverage that is composed of your choice of milk, sweeteners, and matcha powder, which gives it a distinct green color. One sip may be enough to get you hooked. " 
+    }
+]
+
+MenuItem.create(menu_item)
